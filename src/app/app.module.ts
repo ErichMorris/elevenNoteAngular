dc4aa7ea8251b1b9cb154581209824a8fe3a7010
Erich Morris <emorris1692@gmail.com>
2019-04-28 17:13:35 -0400

    finished with authguard errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,77 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { 
+  MatToolbarModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatTableModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
+import { NotesService } from './services/notes.service';
+import { NoteIndexComponent } from './components/note/note-index/note-index.component';
+import { NoteCreateComponent } from './components/note/note-create/note-create.component';
+import { NoteDetailComponent } from './components/note/note-detail/note-detail.component';
+import { NoteEditComponent } from './components/note/note-edit/note-edit.component';
+import { NoteDeleteComponent } from './components/note/note-delete/note-delete.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes = [
+  { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent},
+  { 
+    path: 'notes', canActivate: [AuthGuard] , children: [
+      { path: '', component: NoteIndexComponent },
+      { path: 'create', component: NoteCreateComponent },
+      { path: 'detail/:id', component: NoteDetailComponent },
+      { path: 'edit/:id', component: NoteEditComponent},
+      { path: 'delete/:id', component: NoteDeleteComponent}
+    ]
+  },
+  { path: '**', component: RegistrationComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    LoginComponent,
+    NoteIndexComponent,
+    NoteCreateComponent,
+    NoteDetailComponent,
+    NoteEditComponent,
+    NoteDeleteComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    NotesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/note/note-index/note-index.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NotesService } from '../../../services/notes.service';
+import { Note } from '../../../models/Note';
+import { MatTableDataSource } from '@angular/material';
+
+
+@Component({
+  selector: 'app-note-index',
+  templateUrl: './note-index.component.html',
+  styleUrls: ['./note-index.component.css']
+})
+export class NoteIndexComponent implements OnInit {
+
+  constructor(private _noteService: NotesService) { }
+
+  ngOnInit() {
+    this._noteService.getNotes().subscribe((notes: Note[]) => {
+    });
+  }
+  columnNames = ['details', 'NoteId', 'Title', 'IsStarred', 'CreatedUtc', 'buttons'];
+  dataSource: MatTableDataSource<Note>;
+}
